c204b9f97114941bf93fa922cf419ff2502384a0
RajSolai <msraj085@gmail.com>
2021-05-05 15:59:28 +0530

    added CORS config

--- src/main/java/com/github/rajsolai/yrcwebapi/YrcwebapiApplication.java
+++ src/main/java/com/github/rajsolai/yrcwebapi/YrcwebapiApplication.java
@@ -2,9 +2,23 @@ package com.github.rajsolai.yrcwebapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class YrcwebapiApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(YrcwebapiApplication.class, args);
